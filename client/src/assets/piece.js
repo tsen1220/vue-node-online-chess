74babfb8eefcb483d1a252cfacdcb96ab12ff4d5
unknown <tsen1220.us@gmail.com>
2020-08-24 23:45:05 +0800

    Indentation.

--- client/src/assets/piece.js
+++ client/src/assets/piece.js
@@ -7,47 +7,37 @@ function drawPieceAtBoard(list, row, column) {
       Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265F;</div>`;
       break;
     case -2:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265C;</div>
-         `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265C;</div>`;
       break;
     case -3:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265E;</div>
-               `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265E;</div> `;
       break;
     case -4:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265D;</div>
-               `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265D;</div>`;
       break;
     case -5:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265B;</div>
-               `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265B;</div>`;
       break;
     case -6:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265A;</div>
-                       `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x265A;</div>`;
       break;
     case 1:
       Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2659;</div>`;
       break;
     case 2:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2656;</div>
-               `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2656;</div>`;
       break;
     case 3:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2658;</div>
-                     `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2658;</div>`;
       break;
     case 4:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2657;</div>
-                     `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2657;</div>`;
       break;
     case 5:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2655;</div>
-                     `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2655;</div>`;
       break;
     case 6:
-      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2654;</div>
-                             `;
+      Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' >&#x2654;</div>`;
       break;
     case 0:
       Domelement.innerHTML = `<div style='font-size:60px;text-align:center;' ></div>`;
